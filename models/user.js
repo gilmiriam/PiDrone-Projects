760fb6298a46967676e0e45dd0d591ab58dab94c
gilmiriam <gil.miriam.deljulia@gmail.com>
2017-06-02 09:24:34 +0200

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -4,5 +4,34 @@ module.exports = mongoose.model('User', {
     id: String,
     username: String,
     email: String,
-    password: String
+    password: String,
+    about: String,
+    address: String,
+    city: String,
+    first_name: String,
+    last_name: String,
+    company: String,
+    email: String,
+    country: String,
+    zip: String,
+    images: {
+        path: Array,
+        latitude: Array,
+        longuitud: Array,
+        temperature: Array,
+        presion: Array,
+        humidity: Array
+    },
+    videos: {
+        path: Array,
+        latitude: Array,
+        longuitud: Array,
+        temperature: Array,
+        presion: Array,
+        humidity: Array
+    },
+    notas: {
+        titulo: Array,
+        content: Array
+    }
 });
